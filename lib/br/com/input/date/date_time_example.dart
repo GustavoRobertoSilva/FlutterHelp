5979415240e9eb47cdbfb535c777a3bca18dc636
Gustavo Roberto Silva <gustaoroberto@gmail.com>
2020-10-06 06:18:00 -0300

    Teste

---
+++ lib/br/com/input/date/date_time_example.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class DateTimeExamplePage extends StatefulWidget {
+  DateTimeExamplePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _DateTimeExamplePageState createState() => _DateTimeExamplePageState();
+}
+
+class _DateTimeExamplePageState extends State<DateTimeExamplePage> {
+  String _selectedDate = 'Tap to select date';
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime d = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2015),
+      lastDate: DateTime(2020),
+    );
+    if (d != null)
+      setState(() {
+        _selectedDate = new DateFormat.yMMMMd("en_US").format(d);
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return new MaterialApp(
+        home:  Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Container(
+              decoration: const BoxDecoration(
+                  border: Border(
+                    top: BorderSide(width: 1.0, color: Colors.black),
+                    left: BorderSide(width: 1.0, color: Colors.black),
+                    right: BorderSide(width: 1.0, color: Colors.black),
+                    bottom: BorderSide(width: 1.0, color: Colors.black),
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(5))
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    InkWell(
+                      child: Text(
+                          _selectedDate,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Color(0xFF000000))
+                      ),
+                      onTap: (){
+                        _selectDate(context);
+                      },
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.calendar_today),
+                      tooltip: 'Tap to open date picker',
+                      onPressed: () {
+                        _selectDate(context);
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
